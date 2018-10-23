fc7ca3d2d2cda9221626cb02a2c3a56faf97f814
Yeski's Legion <Yeski's Legion@LAPTOP-T5591HGH>
2018-10-23 23:28:57 +0700

    stream

--- 175314105_susu/src/Model/Pasien.java
+++ 175314105_susu/src/Model/Pasien.java
@@ -4,9 +4,14 @@
  * and open the template in the editor.
  */
 package Model;
-
+//import com.sun.istack.internal.logging.Logger;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+//import java.util.logging.Level;
 
 /**
  *
@@ -24,14 +29,66 @@ public final class Pasien {
         return null;
     }
     public static void simpanDaftarPasien(File file) {
-        getDaftarPasien();
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(file);
+            for (int i = 0; i < daftarPasienKlinik.size(); i++) {
+                String data = daftarPasienKlinik.get(i).toString();
+                fos.write(data.getBytes());
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+//            Logger.getLogger(Pasien.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (IOException ex) {
+//            Logger.getLogger(Pasien.class.getName()).log(Level.SEVERE, null, ex);
+System.out.println(ex.getMessage());
+        } finally {
+            try {
+                fos.close();
+
+            } catch (IOException ex) {
+//                Logger.getLogger(Pasien.class.getName()).log(Level.SEVERE, null, ex);
+System.out.println(ex.getMessage());            }
+        }
     }
-    public static Object getDaftarPasien() {
-        return null;
+    public static void bacaDaftarPasien(File file) {
+        FileInputStream fis = null;
+        String hasil = "";
+        int dataInt;
+        boolean nama = false;
+        boolean alamat = false;
+        Pasien temp = new Pasien();
+        try {
+            fis = new FileInputStream(file);
+            while ((dataInt = fis.read()) != -1) {
+                if ((char) dataInt != '\n') {
+                    if ((char) dataInt != '\t') {
+                        hasil = hasil + (char)dataInt;
+                    } else if (nama == false) {
+                        temp.setNama(hasil);
+                        nama = true;
+                        hasil = "";
+                    } else if (alamat == false) {
+                        temp.setAlamat(hasil);
+                        alamat = true;
+                        hasil = "";
+                    }
+                }
+            }
+        } catch (FileNotFoundException ex) {
+             System.out.println(ex.getMessage());
+
+        } catch (IOException ex) {
+             System.out.println(ex.getMessage());
+        }
+    }
+    public static ArrayList<Pasien> getDaftarPasien() {
+        return daftarPasienKlinik;
     }
     @Override
     public String toString() {
-        return "Pasien : " + "Nama = " + nama + " , Alamat = " + alamat ;
+        return noRekamMedis+"\t"+nama + "\t" + alamat + "\n";
     }
     private String noRekamMedis,nama,alamat,tempatLahir; //deklarasi variabel bertipe data String dan bersifat private
     private int tanggalLahir,bulanLahir,tahunLahir; //deklarasi variabel bertipe data integer dan bersifat privat
